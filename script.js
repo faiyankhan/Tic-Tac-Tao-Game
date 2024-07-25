529abf596435d73ed9e225e0b6097602ad330850
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-07-25 08:07:13 +0000

    Functionality for Reset and n N Game Button Addeded

--- script.js
+++ script.js
@@ -60,3 +60,21 @@ const showWinner = (winner) => {
     msg_container.classList.remove("hide");
     disableBoxes();
 };
+
+const  checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
+}
+
+
+newGame_btn.addEventListener("click", resetGame);
+reset_btn.addEventListener("click", resetGame);

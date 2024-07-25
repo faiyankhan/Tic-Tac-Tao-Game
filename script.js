607ad7b4c55ac1c2e03329a7fe21f5c4406d97e7
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-07-25 07:57:31 +0000

    Multiple Variables are Declared

---
+++ script.js
@@ -0,0 +1,5 @@
+let boxes = document.querySelectorAll(".box");
+let reset_btn = document.querySelector("#reset-button");
+let newGame_btn = document.querySelector("#newGame-button");
+let msg_container = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");

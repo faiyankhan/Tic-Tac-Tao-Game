28499635799e53d140b8d29425fd1cdfd04907d4
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-07-25 07:58:57 +0000

    Winpatterns using array added

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let reset_btn = document.querySelector("#reset-button");
 let newGame_btn = document.querySelector("#newGame-button");
 let msg_container = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+
+
+let turn_O = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
